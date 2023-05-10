6619abf4a44a7de31351162914469ac39454df6e
JP <alias99002@outlook.com>
2023-05-10 14:57:11 -0500

    header and sidebar component created

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,20 @@
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+export function Sidebar() {
+  return (
+    <div className='p-2 md:p-10 py-6 overflow-y-auto border-b border-indigo-500/50'>
+        <div className='flex flex-col items-center justify-center mb-10'>
+        <DocumentMagnifyingGlassIcon className='h-16 w-16 text-indigo-500'/>
+        <h1 className='hidden md:inline text-center text-3xl mt-2 font-semibold'>Web Scrapper</h1>
+        <h2></h2>
+        </div>
+        <ul>
+            {/* Row */}
+            {/* Row */}
+            {/* Row */}
+            {/* Row */}
+            {/* Row */}
+        </ul>
+    </div>
+  );
+}
